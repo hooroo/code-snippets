0b50beef93bb76e0437a8052c375749ab525e81e
jonathanly <jonathanlyhv@gmail.com>
2018-01-12 14:48:15 +1100

    Generates User model via rails command

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,2 +1,11 @@
 FactoryBot.define do
+  factory :user do
+    username "MyString"
+    email "MyString"
+    password "MyString"
+    first_name "MyString"
+    last_name "MyString"
+    created_at "2018-01-12 14:40:14"
+    updated_at "2018-01-12 14:40:14"
+  end
 end

85107ca03f2c3d86df1328e4ab145ad2a111f0b7
jonathanly <jonathanlyhv@gmail.com>
2018-01-12 14:59:35 +1100

    Adds validation to model attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+
+  validates :username, presence: true, uniqueness: true, length: { minimum: 4 }
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
 end

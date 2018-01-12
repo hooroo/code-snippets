adec05eb11ef57f2ccbdaea393bd8eed20494b5e
jonathanly <jonathanlyhv@gmail.com>
2018-01-12 15:29:33 +1100

    Renames password column to password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20180112034014) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.string "first_name"
     t.string "last_name"
     t.datetime "created_at", null: false

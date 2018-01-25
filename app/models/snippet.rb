43ccda6bcaa560ace59e7636a3dc7bc740d08ff4
jonathanly <jonathanlyhv@gmail.com>
2018-01-25 16:50:43 +1100

    Validates snippet attributes

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,4 +1,6 @@
 class Snippet < ApplicationRecord
   belongs_to :user
   has_many :tags, through: :snippet_tags
+
+  validates :title, :body, presence: true
 end
--- spec/models/snippet_spec.rb
+++ spec/models/snippet_spec.rb
@@ -1,4 +1,8 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Snippet, type: :model do
+  context "validations" do
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:body) }
+  end
 end

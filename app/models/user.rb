27e597d5dac52372d9e2e04ae146d95c92fc8477
jonathanly <jonathanlyhv@gmail.com>
2018-02-05 16:10:47 +1100

    Adds one-to-many relation for user and snippet

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :snippets
+
   validates :username, presence: true, uniqueness: true, length: { minimum: 4 }
   validates :email, presence: true, uniqueness: true
   validates :password, length: { minimum: 8 }

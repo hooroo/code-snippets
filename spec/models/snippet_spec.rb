de298311f32a8ab6c8d4db14ded91cd011069d07
jonathanly <jonathanlyhv@gmail.com>
2018-02-05 16:10:16 +1100

    Fixes and updates snippet factory and spec

--- spec/models/snippet_spec.rb
+++ spec/models/snippet_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe Snippet, type: :model do
+RSpec.describe Snippet do
   context "validations" do
     it { is_expected.to validate_presence_of(:title) }
     it { is_expected.to validate_presence_of(:body) }

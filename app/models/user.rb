0f4e28af7ca141b85a50fbdd9d6e9ad2aba2aa32
jonathanly <jonathanlyhv@gmail.com>
2018-02-05 15:03:41 +1100

    User#password validation is already handled by `has_secure_password`

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true, length: { minimum: 4 }
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 8 }
+  validates :password, length: { minimum: 8 }
   validates :first_name, presence: true
   validates :last_name, presence: true
 end

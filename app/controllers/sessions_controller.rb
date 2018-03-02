9fac0a605d794d0b9962a8b1bf3f5661848633dc
jonathanly <jonathanlyhv@gmail.com>
2018-03-02 15:26:39 +1100

    Adds Session#new & #create controller actions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+    if current_user
+      redirect_to user_path(current_user), alert: t(".flash.alert")
+    end
+  end
+
+  def create
+    if user&.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user), notice: t(".flash.notice")
+    else
+      flash[:alert] = t(".flash.alert")
+      render :new
+    end
+  end
+
+  private
+
+  def user
+    @user ||= User.find_by(email: params[:session][:email])
+  end
+end

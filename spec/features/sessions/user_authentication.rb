daf70ccbef0c5ebceb2c944b76ee9496c7b3926f
jonathanly <jonathanlyhv@gmail.com>
2018-03-05 16:33:49 +1100

    Adds user log out feature spec

--- spec/features/sessions/user_authentication.rb
+++ spec/features/sessions/user_authentication.rb
@@ -14,4 +14,18 @@ feature "user authentication" do
     )
     expect(page).to have_text(I18n.t("sessions.create.flash.notice"))
   end
+
+  scenario "succesfully log out" do
+    user = create(:user)
+
+    visit(login_path)
+    fill_in("session_email", with: user.email)
+    fill_in("session_password", with: user.password)
+    click_on(I18n.t("sessions.new.submit"))
+
+    click_on(I18n.t("sessions.links.sign_out"))
+
+    expect(page).to have_text(I18n.t("sessions.new.title"))
+    expect(page).to have_text(I18n.t("sessions.destroy.flash.notice"))
+  end
 end

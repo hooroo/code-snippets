f36b5e84b8e090fe83a858a398ae9e30e18b067f
jonathanly <jonathanlyhv@gmail.com>
2018-03-05 13:26:10 +1100

    Adds sign in with invalid credentials controller spec

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,56 @@
+require "rails_helper"
+
+describe SessionsController do
+  context "POST #create" do
+    context "with invalid credentials" do
+      it "does not create a session" do
+        user = build_stubbed(:user)
+
+        post :create, params: { session: { email: user.email } }
+
+        expect(session[:user_id]).to be_nil
+      end
+
+      it "renders the :new template" do
+        user = build_stubbed(:user)
+
+        post :create, params: { session: { email: user.email } }
+
+        expect(response).to render_template(:new)
+      end
+
+      it "displays a flash alert" do
+        user = build_stubbed(:user)
+
+        post :create, params: { session: { email: user.email } }
+
+        expect(flash[:alert]).to match I18n.t("sessions.create.flash.alert")
+      end
+    end
+  end
+<<<<<<< HEAD:spec/controllers/sessions_controller.rb
+=======
+
+  context "GET #new" do
+    context "when a user is already logged in" do
+      it "renders the user :show template" do
+        user = build_stubbed(:user)
+        allow(controller).to receive(:current_user).and_return(user)
+
+        get :new
+
+        expect(response).to redirect_to(user_path(user))
+      end
+
+      it "displays a flash alert" do
+        user = build_stubbed(:user)
+        allow(controller).to receive(:current_user).and_return(user)
+
+        get :new
+
+        expect(flash[:alert]).to match I18n.t("sessions.new.flash.alert")
+      end
+    end
+  end
+>>>>>>> 2c70638... fixup! Adds sign in with invalid credentials controller spec:spec/controllers/sessions_controller_spec.rb
+end
